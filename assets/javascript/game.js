bd493c0e837af7a092dcb0d83bcd4e3efbf56775
Sandman105 <cpossandon@gmail.com>
2019-09-29 00:36:24 -0400

    some basic buttons, some pseudo-code

---
+++ assets/javascript/game.js
@@ -0,0 +1,53 @@
+//VARIABLES===================================================
+
+
+//* Notes 
+// 1.)Some basic pseudo-code, so we need to create some variables, empty one to store the integers that are generated.
+// 2.) We need to initialize the program, so basically rest or clear the program when we start or we start a new game. So we need a intialize function to reset game and when game page is loaded.
+// 3.) We'll need to create some tags for the button, so we can refer to them for the on click event.
+// 4.)Continue to write your pseudo-code as you were the user. You're sitting infront of the keyboard, what do you do,what are you pressing, what action does it take when you push the button. What happens when an action is already taken, what is the next step in the program. Review the calulator notes I took about each block of code as guidence and also refer back to psychic game for tips there too, as far as sequence. Essentially, it feels like we work backwards in when defining each block of code by starting if a variable is true, then set to false.
+// 5.) Need a method to generate a random number for the numbers on the crystals and the random number that's spawned that we need to add up to.
+// 6.) We need to assign those values to a the crystal buttons, so we need the .val() method  
+// 7.)Try also too to not focus too much on creating every variable in the beginning, focus on getting one thing working.
+
+
+
+
+
+
+
+$(document).ready(function () {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
